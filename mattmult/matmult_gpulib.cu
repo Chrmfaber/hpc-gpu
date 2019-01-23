7f10a0f3e6bad2fbcc52ce38b2270c39c252ffbb
Hedda Berg <berg.hedda@gmail.com>
2019-01-23 13:41:32 +0100

    no message

--- mattmult/matmult_gpulib.cu
+++ mattmult/matmult_gpulib.cu
@@ -1,21 +1,20 @@
 #include <cublas_v2.h>
 
-extern "C" { void matmult_gpulib(double *A, double *B, double *C, int m, int k, int n) {
+extern "C" { void matmult_gpulib(double *A, double *B, double *C, int m, int n, int k) {
 
      // Create a handle for CUBLAS
    	 cublasHandle_t handle;
    	 cublasCreate(&handle);
 
-      const double alf = 1;
-      const double bet = 0;
-
+      const double alf = 1.0;
+      const double bet = 0.0;
       const double *alpha = &alf;
       const double *beta = &bet;
 
       int lda = m, ldb = k, ldc = m;
 
      // Do the multiplication
-     cublasDgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, n, m, k, alpha, A, lda, B, ldb, beta, C, ldc);
+     cublasDgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, m, n, k, alpha, A, lda, B, ldb, beta, C, ldc);
 
      // Destroy the handle
      cublasDestroy(handle);
