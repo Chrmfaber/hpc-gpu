277d2d24fb9d23d00d3b26a5403c77f9dddd0d3e
Hedda Berg <berg.hedda@gmail.com>
2019-01-24 12:51:06 +0100

    no message

--- mattmult/R-code/matmult_gpu.R
+++ mattmult/R-code/matmult_gpu.R
@@ -6,6 +6,7 @@ gpu_matmult.gpu2 <- read.table("~/Google Drive/10.semester/02614 HPC/Assignment 
 gpu_matmult.gpu3_col <- read.table("~/Google Drive/10.semester/02614 HPC/Assignment 3/GIT3/mattmult/Data_v1/gpu_matmult.gpu3Col.dat", quote="\"", comment.char="")
 gpu_matmult.gpu3_row <- read.table("~/Google Drive/10.semester/02614 HPC/Assignment 3/GIT3/mattmult/Data_v1/gpu_matmult.gpu3Row.dat", quote="\"", comment.char="")
 gpu_matmult.gpu4 <- read.table("~/Google Drive/10.semester/02614 HPC/Assignment 3/GIT3/mattmult/Data_v1/gpu_matmult.gpu4.dat", quote="\"", comment.char="")
+gpu_matmult_gpu5_bs16 <- read.table("~/Google Drive/10.semester/02614 HPC/Assignment 3/GIT3/mattmult/Data_v1/gpu5_matmult_bs16.dat", quote="\"", comment.char="")
 
 gpu_matmult.gpulib <- gpu_matmult.gpulib[1:2]
 colnames(gpu_matmult.gpulib) <- c("Memory","Flops")
@@ -28,8 +29,11 @@ gpu_matmult.gpu3_row$Type <- "matmult_gpu3_row"
 gpu_matmult.gpu4 <- gpu_matmult.gpu4[1:2]
 colnames(gpu_matmult.gpu4) <- c("Memory","Flops")
 gpu_matmult.gpu4$Type <- "matmult_gpu4"
+gpu_matmult_gpu5_bs16 <- gpu_matmult_gpu5_bs16[1:10,1:2]
+colnames(gpu_matmult_gpu5_bs16) <- c("Memory","Flops")
+gpu_matmult_gpu5_bs16$Type <- "matmult_gpu5:16"
 
-gpu_matmult <- rbind(gpu_matmult.gpu1,gpu_matmult.gpu2,gpu_matmult.gpu3_row,gpu_matmult.gpu3_col,gpu_matmult.gpu4,gpu_matmult.gpulib,gpu_matmult.lib)
+gpu_matmult <- rbind(gpu_matmult.gpu1,gpu_matmult.gpu2,gpu_matmult.gpu3_row,gpu_matmult.gpu3_col,gpu_matmult.gpu4,gpu_matmult.gpulib,gpu_matmult.lib,gpu_matmult_gpu5_bs16)
 
 gpu_matmult$Flops <- gpu_matmult$Flops/1000
 gpu_matmult$Memory <- gpu_matmult$Memory/1000
@@ -41,5 +45,5 @@ ggplot(data=gpu_matmult, aes(x=Memory, y=Flops,col=Type)) +
 ggplot(data=gpu_matmult, aes(x=Memory, y=Flops,col=Type)) + ylim(0,0.16)+xlim(0,0.03)+
   geom_line()+geom_point()+labs(x="Memory(MB)", y="GFlops/s")
 
-ggplot(data=gpu_matmult, aes(x=Memory, y=Flops,col=Type)) + ylim(0,65)+xlim(0,4.5)+
+ggplot(data=gpu_matmult, aes(x=Memory, y=Flops,col=Type)) + ylim(0,65)+xlim(0,2.2)+
   geom_line()+geom_point()+labs(x="Memory(MB)", y="GFlops/s")
