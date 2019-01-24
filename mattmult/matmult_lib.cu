b9a02ee8a0f87aedfae87cd2c766ee15df53dbcc
Hedda Berg <berg.hedda@gmail.com>
2019-01-24 10:18:13 +0100

    no message

--- mattmult/matmult_lib.cu
+++ mattmult/matmult_lib.cu
@@ -1,9 +1,23 @@
-extern "C" { #include <cblas.h>
+extern "C" {
+#include <cblas.h>
 
-void matmult_lib(int m, int n, int k, double **A, double **B, double **C) {
-  double alpha = 1.0, beta = 0.0;
+void matmult_lib(int m, int n, int k, double *A, double *B, double *C) {
 
-  cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, m, n, k, alpha, A[0],
-              k, B[0], n, beta, C[0], n);
+    const double alf = 1.0;
+    const double bet = 0.0;
+    const double *alpha = &alf;
+    const double *beta = &bet;
+
+    int lda = k, ldb = n, ldc = n;
+
+
+    /* Ensure that all elements of C is 0 */
+  	for (int i=0; i<m ; i++){
+  		for (int j=0; j<n ; j++){
+  			C[i*n + j] = 0;
+  		}
+
+  	}
+  cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, m, n, k, alpha, A, lda, B, ldb, beta, C, ldc);
 }
 }
